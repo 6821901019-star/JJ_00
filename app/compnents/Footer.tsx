f99d3350b69fb2d46650726fcdc2ef3104c37247
6821901019-star <6821901019@cdti.ac.th>
2026-06-04 10:31:10 +0700

    updete

---
+++ app/compnents/Footer.tsx
@@ -0,0 +1,8 @@
+import Link from "next/link";
+export default function footer() {
+    return (
+        <footer className="bg-gray-800 text-white p-4 mt-8">
+            <p>&copy; 2026 โดย เจณสุฎาณ์ </p>
+        </footer>
+    )
+}
