26268b5b9dd8f931194da7d0ded0edaa3dc0add3
6821901019-star <6821901019@cdti.ac.th>
2026-06-25 10:26:43 +0700

    updete

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,21 @@
+import { redirect } from "next/navigation";
+import { getSession } from "@/lib/session";
+import { count } from "console";
+
+export default async function Dashboard(){
+
+    const user = await getSession();
+
+    if (!user){
+        redirect("/login");
+    }
+
+    return(
+        <div>
+
+            <h2> Dashbosrd </h2>
+            <p>Welcom: {user.name as string}</p>
+            <p>Role: {user.role as string}</p>
+        </div>
+    );
+}
